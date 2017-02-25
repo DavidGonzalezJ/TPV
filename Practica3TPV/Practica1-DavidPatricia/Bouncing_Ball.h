1e680332de5d25bda58662fbd7b9dda467b296db
David González <davgon12@ucm.es>
2017-02-25 17:59:51 +0100

    Casi igual, pero David cree que ha hecho algo

--- Practica3TPV/Practica1-DavidPatricia/Bouncing_Ball.h
+++ Practica3TPV/Practica1-DavidPatricia/Bouncing_Ball.h
@@ -13,8 +13,6 @@ public:
 private:
 	bool expl;
 	int dx, dy;
-	static const int PVIS = 70; //La probabilidad de que un globo esté visible
-	static const int PDES = 30; //La probabilidad de que se desinfle
 	static const int AP = 10; //Puntos que resta cada vez que se mueve
 	void sentido();
 };
